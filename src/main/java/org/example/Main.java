c001d048ed34bec2552bb4d1d95a1d94ff68492a
netgodRUS <108462820+netgodRUS@users.noreply.github.com>
2022-10-21 20:24:15 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i;
+        for(i=1;i<=3;i++)
+        {
+            System.out.print("Hello world!");
     }
 }
